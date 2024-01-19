76595c748b3c52595911cf8140331ef09c8541a0
aditi <additigupta366@gmail.com>
2024-01-20 01:13:32 +0530

    fixed

--- Medicare-main/src/components/DoctorList.jsx
+++ Medicare-main/src/components/DoctorList.jsx
@@ -5,21 +5,21 @@ const DoctorList = ({ doctor }) => {
     const navigate =useNavigate();
   return (
     <>
-      <div className="card m-2" style={{cursor:'Pointer'}} onClick={()=> navigate(`/doctor/book-appointment/${doctor._id}`)}>
-        <div className="card-header">
+      <div className="card my-4  mx-2 border-2 p-2 rounded-lg bg-gradient-to-r from-cyan-600 to-purple-600 shadow-md" style={{cursor:'Pointer'}} onClick={()=> navigate(`/doctor/book-appointment/${doctor._id}`)}>
+        <div className="card-header text-start font-[500] text-[25px] text-yellow-400 px-2 shadow my-2">
           Dr. {doctor.firstName} {doctor.lastName}
         </div>
         <div className="card-body">
-            <p>
+            <p className="text-white text-start px-2">
                 <b>Specialization</b> {doctor.specialization}
             </p>
-            <p>
+            <p className="text-white text-start px-2">
                 <b>Experience</b> {doctor.experience}
             </p>
-            <p>
+            <p className="text-white text-start px-2">
                 <b>Fees per Consultation</b> {doctor.feesperConsultation}
             </p>
-            <p>
+            <p className="text-white text-start px-2">
                 <b>Timings</b> {doctor.timings[0]} - {doctor.timings[1]}
             </p>
         </div>
